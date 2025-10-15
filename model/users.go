a75478583376ab8d00436411f55964d49025168a
ghaidafasya24 <ghaidafasya5@gmail.com>
2025-10-15 14:48:59 +0700

    add func regis, login, struct users, n route

---
+++ model/users.go
@@ -0,0 +1,12 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Users struct {
+	ID              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Role            string             `json:"role,omitempty" bson:"role,omitempty" gorm:"unique;not null"`
+	Username        string             `json:"username,omitempty" bson:"username,omitempty" gorm:"unique;not null"`
+	PhoneNumber     string             `json:"phone_number,omitempty" bson:"phone_number,omitempty" gorm:"unique;not null"`
+	Password        string             `json:"password,omitempty" bson:"password,omitempty"`
+	ConfirmPassword string             `json:"confirm_password,omitempty" bson:"confirm_password,omitempty"`
+}
