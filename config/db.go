04d5ec14bcd91215e5036072faa1d1c1e5c19a3b
ghaidafasya24 <ghaidafasya5@gmail.com>
2025-10-16 15:06:53 +0700

    add dbname

--- config/db.go
+++ config/db.go
@@ -13,7 +13,7 @@ var MongoString string = os.Getenv("MONGOSTRING")
 
 var DBUlbimongoinfo = atdb.DBInfo{
 	DBString: MongoString,
-	DBName:   "",
+	DBName:   "inventorymuseum",
 }
 
 var Ulbimongoconn = atdb.MongoConnect(DBUlbimongoinfo)
